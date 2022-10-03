698a5bec26b0382efc28faba667ada16ed970d21
Lekret <lekret399@mail.ru>
2022-10-03 05:27:09 +0300

    Scores

--- Assets/Scripts/AiAction.cs
+++ Assets/Scripts/AiAction.cs
@@ -6,11 +6,13 @@ public abstract class AiAction : ScriptableObject
     [SerializeField] private string _name;
     [SerializeField] private float _score;
 
+    public Consideration[] Considerations => _considerations;
+    
     public float Score
     {
         get => _score;
         set => _score = Mathf.Clamp01(value);
     }
-
+    
     public abstract void Execute();
 }
--- Assets/Scripts/AiBrain.cs
+++ Assets/Scripts/AiBrain.cs
@@ -4,13 +4,39 @@ public class AiBrain : MonoBehaviour
 {
     public AiAction BestAction { get; private set; }
 
-    public void ScoreAction(AiAction action)
+    public void UpdateBestAction(AiAction[] actions)
     {
-        
+        var bestScore = 0f;
+        var bestActionIdx = 0;
+        for (var i = 0; i < actions.Length; i++)
+        {
+            if (ScoreAction(actions[i]) > bestScore)
+            {
+                bestActionIdx = i;
+                bestScore = actions[i].Score;
+            }
+        }
+
+        BestAction = actions[bestActionIdx];
     }
 
-    public void DecideBestAction(AiAction[] actions)
+    private static float ScoreAction(AiAction action)
     {
-        
+        var score = 1f;
+        foreach (var consideration in action.Considerations)
+        {
+            var considerationScore = consideration.ScoreConsideration();
+            score *= considerationScore;
+            if (score == 0)
+            {
+                action.Score = score;
+                return action.Score;
+            }
+        }
+
+        var modFactor = 1 - 1 / action.Considerations.Length;
+        var makeupValue = (1 - score) * modFactor;
+        action.Score = score + score * makeupValue;
+        return action.Score;
     }
 }
